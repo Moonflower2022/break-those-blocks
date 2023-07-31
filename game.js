e5eedfb4e4d231bfd2d31b52142c4cc59d32936d
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2023-07-30 21:57:33 -0700

    changes

--- game.js
+++ game.js
@@ -1593,7 +1593,8 @@ function draw() {
 			strokeWeight(0)
 			textFont(oswald, 24)
 			text("I recommend using Ctrl/Cmd + and - to adjust the display size and playing in fullscreen mode.", windowWidth - (windowWidth / 4 + 100) - 225, 285, 450, 600)
-			text("If you get confused on how to play, click on the question mark in the top right of the screen.", windowWidth - (windowWidth / 4 + 100) - 225, 370, 450, 600)
+			//text("If you get confused on how to play, click on the question mark in the top right of the screen.", windowWidth - (windowWidth / 4 + 100) - 225, 370, 450, 600)
+			text("Shoot the balls and break the blocks, and use unique powerups to help you! Also, try out all of the modes.", windowWidth - (windowWidth / 4 + 100) - 225, 370, 450, 600)
 			text("Features in progress: Leaderboards and Tutorial", windowWidth - (windowWidth / 4 + 100) - 225, 455, 450, 600)
 			text("If you notice any bugs, experience any issues, or have any questions, email me at blocksbreak15@gmail.com. I will try to respond as fast as I can.", windowWidth - (windowWidth / 4 + 100) - 225, 510, 450, 600)
 		}
